de59ff9e7857b7a9b7d09d42045fc88276333c51
wez <wmeyer85@verizon.net>
2015-12-07 17:05:05 -0500

    assign4 commit

--- src/java/NewCustomer/NewCustomerServlet.java
+++ src/java/NewCustomer/NewCustomerServlet.java
@@ -6,6 +6,9 @@ import toba.data.UserDB;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.security.NoSuchAlgorithmException;
+
+import toba.util.PasswordUtil;
 
 
 public class NewCustomerServlet extends HttpServlet {
@@ -83,5 +86,40 @@ public class NewCustomerServlet extends HttpServlet {
                           HttpServletResponse response) 
                           throws ServletException, IOException {
         doPost(request, response);
+        
+        // get parameters from the request
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        String message;
+        try {
+            PasswordUtil.checkPasswordStrength(password);
+            message = "";
+        } catch (Exception e) {
+            message = e.getMessage();
+        }
+        request.setAttribute("message", message);        
+        
+        // hash and salt password
+        String hashedPassword;
+        String salt = "";
+        String saltedAndHashedPassword;
+        try {
+            hashedPassword = PasswordUtil.hashPassword(password);
+            salt = PasswordUtil.getSalt();
+            saltedAndHashedPassword = PasswordUtil.hashAndSaltPassword(password);                    
+            
+        } catch (NoSuchAlgorithmException ex) {
+            hashedPassword = ex.getMessage();
+            saltedAndHashedPassword = ex.getMessage();
+        }
+        request.setAttribute("hashedPassword", hashedPassword);
+        request.setAttribute("salt", salt);
+        request.setAttribute("saltedAndHashedPassword", saltedAndHashedPassword);
+        
+        String url = "/index.jsp";
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
     }    
 }
